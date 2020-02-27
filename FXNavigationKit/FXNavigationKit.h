b0b72a8ec69b8b1e6f0cb54ba5659a9fcb1bc5c4
wupengfei <1569485690@qq.com>
2020-02-28 01:30:27 +0800

    Initial commit

---
+++ FXNavigationKit/FXNavigationKit.h
@@ -0,0 +1,19 @@
+//
+//  FXNavigationKit.h
+//  FXNavigationKit
+//
+//  Created by Mr.wu on 2020/1/27.
+//  Copyright © 2020 Mr.wu. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for FXNavigationKit.
+FOUNDATION_EXPORT double FXNavigationKitVersionNumber;
+
+//! Project version string for FXNavigationKit.
+FOUNDATION_EXPORT const unsigned char FXNavigationKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FXNavigationKit/PublicHeader.h>
+
+
---
+++ Sources/FXNavigationBar.swift
@@ -0,0 +1,21 @@
+//
+//  FXNavigationBar.swift
+//  FXNavigationKit
+//
+//  Created by Mr.wu on 2020/1/27.
+//  Copyright © 2020 Mr.wu. All rights reserved.
+//
+
+import UIKit
+
+class FXNavigationBar: UINavigationBar {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
